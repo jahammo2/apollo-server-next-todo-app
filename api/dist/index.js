7ea8de6be713d4e9711d35de30d7650bd75e87ec
Jordan Hammond <jahammo2@gmail.com>
2023-06-20 12:36:58 -0600

    first commit

---
+++ api/dist/index.js
@@ -0,0 +1,29 @@
+import { ApolloServer } from '@apollo/server';
+import { PrismaClient } from '@prisma/client';
+import { startStandaloneServer } from '@apollo/server/standalone';
+// Initialize Prisma Client
+const prisma = new PrismaClient();
+// Define your GraphQL schema
+const typeDefs = `#graphql
+  type Todo {
+    id: ID!
+    text: String!
+    completed: Boolean!
+  }
+
+  type Query {
+    todos: [Todo]!
+  }
+`;
+// Define your resolvers
+const resolvers = {
+    Query: {
+        todos: async () => await prisma.todo.findMany(),
+    },
+};
+// Create the Apollo Server
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+const { url } = await startStandaloneServer(apolloServer, {
+    listen: { path: '/api', port: 3002 },
+});
+console.log(`🚀 Server ready at ${url}`);
